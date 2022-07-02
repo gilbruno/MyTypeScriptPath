0dfa82c7f264ee1fd0715ddd85d13fc11a739c4d
Gilles Bruno <gilles_bruno@gmx.fr>
2022-07-02 18:51:40 +0200

    Example of alias type

---
+++ dist/aliastype.js
@@ -0,0 +1,9 @@
+"use strict";
+const User = {
+    firstName: "John",
+    lastName: "Doe"
+};
+const User2 = {
+    firstName: "Alice",
+    lastName: "Doo"
+};
---
+++ src/aliastype.ts
@@ -0,0 +1,12 @@
+const User: {firstName: string, lastName:string} = {
+    firstName: "John",
+    lastName: "Doe"
+}
+
+
+type UserType = {firstName: string, lastName:string}
+
+const User2: UserType = {
+    firstName: "Alice",
+    lastName: "Doo"
+}
