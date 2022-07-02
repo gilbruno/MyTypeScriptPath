ac6c7b75636160603858a86d19f73a6de1bb20bb
Gilles Bruno <gilles_bruno@gmx.fr>
2022-07-02 18:07:01 +0200

    First set of files for my TS path

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+const compteur = document.querySelector('#compteur')
+
+let i = 0
+
+const increment = (e) => {
+    i++
+
+    compteeur.querySelector('span').innerText = i.toString()
+
+}
+
+compteur.addEventListener('click', increment)
+
