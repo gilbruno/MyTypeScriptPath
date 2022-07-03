9b665704a6b1ebe141ad5280089ee24a339f4b73
Gilles Bruno <gilles_bruno@gmx.fr>
2022-07-03 17:22:09 +0200

    add comment on interface

---
+++ src/interface.ts
@@ -0,0 +1,11 @@
+interface Point {
+    x: number;
+}
+
+//The interface 'Point' can be modified later by adding some extras properties for instance
+// We call it 'Fusion'
+interface Point{
+    y: number;
+}
+
+
