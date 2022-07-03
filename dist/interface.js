b1cd90d49649f03f29b1a62420f7c14a6c870663
Gilles Bruno <gilles_bruno@gmx.fr>
2022-07-03 17:29:32 +0200

    Interface implementation

--- dist/interface.js
+++ dist/interface.js
@@ -1,1 +1,12 @@
 "use strict";
+//Interfacein TS can be implemented exactly like in Java
+class Point2D {
+    constructor() {
+        this.x = 6;
+        this.y = 10;
+    }
+}
+function draw(p) {
+    console.log("Coordinates are : X= " + p.x + " - Y=" + p.y);
+}
+draw(new Point2D());
--- src/interface.ts
+++ src/interface.ts
@@ -8,4 +8,19 @@ interface Point{
     y: number;
 }
 
+interface Coordinates {
+    x: number
+    y: number
+}
 
+//Interfacein TS can be implemented exactly like in Java
+class Point2D implements Coordinates {
+    x = 6
+    y = 10
+}
+
+function draw(p: Coordinates) {
+    console.log("Coordinates are : X= " + p.x + " - Y=" + p.y)
+}
+
+draw(new Point2D())
