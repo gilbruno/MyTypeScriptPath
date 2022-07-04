e89bbe039f6411395052419d080e53849e8465fb
Gilles Bruno <gilles_bruno@gmx.fr>
2022-07-04 21:21:24 +0200

    Examples of enum

---
+++ dist/enum.js
@@ -0,0 +1,10 @@
+"use strict";
+// Enum in TS works exactlylike in Solidity !!
+var STEPS;
+(function (STEPS) {
+    STEPS[STEPS["STEP_1"] = 0] = "STEP_1";
+    STEPS[STEPS["STEP_2"] = 1] = "STEP_2";
+    STEPS[STEPS["STEP_3"] = 2] = "STEP_3";
+})(STEPS || (STEPS = {}));
+const myStep = STEPS.STEP_2;
+console.log(myStep);
---
+++ src/enum.ts
@@ -0,0 +1,12 @@
+// Enum in TS works exactlylike in Solidity !!
+
+enum STEPS {
+    STEP_1,
+    STEP_2,
+    STEP_3
+}
+
+const myStep: STEPS = STEPS.STEP_2
+
+console.log(myStep)
+
